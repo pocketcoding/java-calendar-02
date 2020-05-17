9a5b4be6ebc7a66e3e1cddb47bfd2fe86d6aeb19
mac <mac@macui-MacBookAir.local>
2020-05-17 18:04:27 +0900

    End : while loop with if-condition to end the loop

--- src/abby/calendar/Calendar.java
+++ src/abby/calendar/Calendar.java
@@ -3,27 +3,38 @@ package abby.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxdaysOfMonth(int month) {
-		
-		return 0;
-		
+
+		return MAX_DAYS[month - 1];
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		
 		String PROMPT = "cal > ";
 		Scanner scanner = new Scanner(System.in);
-		System.out.print(PROMPT);
-		int month = scanner.nextInt();
+		Calendar cal = new Calendar();
 		
-		System.out.println(month*month);
-	
+		int month = 1; // we need to initialize the month to compare in the while loop
 		
+		while (true) {
 		
+			System.out.print(PROMPT);
+			month = scanner.nextInt();	
+			
+			if(month == -1) { break; }
+			if(month > 12) { continue; }
+			
+			System.out.printf("The maximum dates of the month %d is %d \n", month, cal.getMaxdaysOfMonth(month));
+
+		}
+
+		System.out.println("Bye~");
+		scanner.close();
+
 	}
 
 }
